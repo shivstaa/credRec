9368cf4d87b1d14ca3709acbf6dd243034029961
shivstaa <shivshahla@gmail.com>
2024-04-21 03:40:45 -0700

    added toggle for logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { BsFillCreditCard2FrontFill } from "react-icons/bs";
 import { useAuth } from "./AuthContext";
 import { auth } from '../../firebase'; // Ensure this path is correct!
@@ -7,10 +7,13 @@ import { signOut } from "firebase/auth";
 
 const Navbar: React.FC = () => {
   const { user, authenticated } = useAuth();
+  const [dropdownOpen, setDropdownOpen] = useState(false);  // State to manage dropdown visibility
 
   const handleLogout = () => {
     signOut(auth).catch(error => console.error('Logout failed', error));
   };
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);  // Toggle function
 
   return (
     <nav className="bg-white shadow-lg">
@@ -27,12 +30,16 @@ const Navbar: React.FC = () => {
             <Link href="/history" className="py-5 px-3 text-gray-700 hover:text-gray-900">History</Link>
             {authenticated ? (
               <div className="relative">
-                <button className="py-5 px-3 text-gray-700 hover:text-gray-900">{user?.email}</button>
-                <div className="absolute right-0 bg-white shadow-md mt-2">
-                  <button className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100" onClick={handleLogout}>
-                    Logout
-                  </button>
-                </div>
+                <button onClick={toggleDropdown} className="py-5 px-3 text-gray-700 hover:text-gray-900">
+                  {user?.email}
+                </button>
+                {dropdownOpen && (
+                  <div className="absolute right-0 bg-white shadow-md mt-2">
+                    <button className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100" onClick={handleLogout}>
+                      Logout
+                    </button>
+                  </div>
+                )}
               </div>
             ) : (
               <Link href="/login" className="py-5 px-3 text-gray-700 hover:text-gray-900">Login</Link>
