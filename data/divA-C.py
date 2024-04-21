d34e8ce91bf3a3191c35381df5592239d251592f
Andrew Kim <andrew.kim435@berkeley.edu>
2024-04-21 02:59:36 -0700

    a-c complete, d-z in progress

---
+++ data/divA-C.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import requests
+
+# Configure ChromeDriver options
+options = Options()
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+options.add_argument('referer=https://www.google.com/')
+options.add_argument('--headless')
+options.add_argument('--enable-third-party-cookies')  # Attempt to allow third-party cookies
+options.add_argument("--log-level=3")
+
+# Initialize WebDriver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+url = 'https://www.cardratings.com/credit-card-list.html#div_D'
+driver.get(url)
+link_element = driver.find_element(By.CLASS_NAME, "div_A")
+link_element.click()
+
+# Use WebDriverWait to ensure the page has loaded the necessary elements
+WebDriverWait(driver, 20).until(
+    #EC.visibility_of_element_located((By.ID, "div_D"))
+    EC.presence_of_element_located((By.ID, "onetrust-consent-sdk"))  # Adjust the ID based on actual content you need
+)
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+soup_A = soup.find(id='cardList')
+
+cards_links = soup_A.find_all("div", {"class": "filterDiv"})
+
+def download_webp_image_to_list(image_url):
+    # Send a GET request to the image URL
+    response = requests.get(image_url)
+    
+    # Check if the request was successful (HTTP status code 200)
+    if response.status_code == 200:
+        # Return the content of the response (the image data)
+        return response.content
+    else:
+        print("Failed to retrieve WebP image. Status code:", response.status_code)
+        return None
+
+unique_card = []
+
+for cardlink in cards_links:
+    element = cardlink.find("a", class_="sh-active-client sh-quidget-rendered")
+    if element:
+        name = element.get('data-rate-name')
+        link = element["href"]
+        creditNeededSpan = cardlink.find("span", class_="credit_needed font-weight-semibold")
+        creditNeeded = creditNeededSpan.text
+        img_element = cardlink.find("img")
+        imagesource = img_element["src"]
+        #image_data = download_webp_image_to_list(imagesource)
+
+    # description = "No description available"  # Default description
+    ul = cardlink.find("ul")
+
+    if not ul: 
+        pass
+
+    if ul:
+        description = ul.text.strip()  # Extract text and strip any excess whitespace
+    
+    if name not in unique_card and description not in unique_card:  # Prevent duplicate card names
+        unique_card.append((name, link, description, creditNeeded, imagesource))  # Store as a tuple for better structure
+        #print(type(image_data))
+# Output the results
+# for card in unique_card:
+#     print(card)
+
+with open('div_A-C.txt', 'w') as f:
+    for line in unique_card:
+        f.write(f"{line}\n")
--- data/temp.py
+++ data/temp.py
@@ -6,20 +6,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import time
+import requests
 from selenium.common.exceptions import TimeoutException
+
 
 # Configure ChromeDriver options
 options = Options()
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+options.add_argument('referer=https://www.google.com/')
 options.add_argument('--headless')
-options.add_argument("--disable-gpu")
+options.add_argument('--enable-third-party-cookies')  # Attempt to allow third-party cookies
 options.add_argument("--log-level=3")
-
 
 # Initialize WebDriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-url = 'https://www.cardratings.com/credit-card-list.html'
+url = 'https://www.cardratings.com/credit-card-list.html#div_D'
 driver.get(url)
+
 
 # List to hold all unique cards from all divs
 all_cards = []
@@ -28,26 +31,43 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 # Loop over the IDs from div_A to div_Z
-for char in range(ord('A'), ord('Z')+1):
+for char in range(ord('D'), ord('Z')+1):
     div_id = f"div_{chr(char)}"
-    print(chr(char))
+    link_element = driver.find_element(By.CLASS_NAME, div_id)
+    link_element.click()
     try:
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, div_id)))
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "onetrust-consent-sdk")))
 
-        current_div = soup.find(id=div_id)
+        current_div = soup.find(id='cardList')
         if current_div:
-            cards_links = current_div.find_all("span", {"class": "sh-active-client sh-quidget-rendered"})
+            cards_links = current_div.find_all("div", {"class": "filterDiv"})
             for cardlink in cards_links:
-                name = cardlink.get('data-rate-name', 'No Name Available')
+                element = cardlink.find("a", class_="sh-active-client sh-quidget-rendered")
+                if element:
+                    name = element.get(0, 'data-rate-name')
+                    link = element["href"]
+                    creditNeededSpan = cardlink.find("span", class_="credit_needed font-weight-semibold")
+                    creditNeeded = creditNeededSpan.text
+                    img_element = cardlink.find("img")
+                    imagesource = img_element["src"]
+                    #image_data = download_webp_image_to_list(imagesource)
                 ul = cardlink.find("ul")
-                description = ul.text.strip() if ul else "No description available"
-                card_info = (div_id, name, description)
-                if (name, description) not in all_cards:
-                    all_cards.append(card_info)
-                    print(name, description)
+
+                if not ul: 
+                    pass
+
+                if ul:
+                    description = ul.text.strip()  # Extract text and strip any excess whitespace
+    
+                if name not in all_cards and description not in all_cards:  # Prevent duplicate card names
+                    all_cards.append((name, link, description, creditNeeded, imagesource))  # Store as a tuple for better structure
+                    #print(type(image_data))
     except TimeoutException:
         print(f"Timeout or element not found for {div_id}")
 
 # Output the results
-for card in all_cards:
-    print(card)
+# for card in all_cards:
+#     print(card)
+with open('div_D-Z.txt', 'w') as f:
+    for line in all_cards:
+        f.write(f"{line}\n")
